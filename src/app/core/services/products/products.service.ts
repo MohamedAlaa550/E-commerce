f620a0fbdc5886cafadc58775e7814d7b51e68d5
MohamedAlaa550 <mohamedalaa.2675@gmail.com>
2025-05-07 19:41:22 +0300

    modify category

--- src/app/core/services/products/products.service.ts
+++ src/app/core/services/products/products.service.ts
@@ -4,18 +4,24 @@ import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient:HttpClient) { }
+  getAllProducts(): Observable<any> {
+    return this.httpClient.get(
+      `${environment.baseUrl}/api/v1/products?page=1&limit=56`
+    );
+  }
 
-getAllProducts():Observable<any>{
-  return this.httpClient.get(`${environment.baseUrl}/api/v1/products?page=1&limit=56`)
-}
+  getSpecificProduct(id: string | null): Observable<any> {
+    return this.httpClient.get(`${environment.baseUrl}/api/v1/products/${id}`);
+  }
 
-getSpecificProduct(id:string | null):Observable<any>{
-  return this.httpClient.get(`${environment.baseUrl}/api/v1/products/${id}`)
-}
-
+  getMoviesDatat(movieName: string): Observable<any> {
+    return this.httpClient.get(
+      `https://www.omdbapi.com/?s=${movieName}&apikey=917bdfc2`
+    );
+  }
 }
--- src/app/pages/allorders/allorders.component.ts
+++ src/app/pages/allorders/allorders.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
+import { debounceTime, distinctUntilChanged, fromEvent, switchMap } from 'rxjs';
+import { ProductsService } from '../../core/services/products/products.service';
 
 @Component({
   selector: 'app-allorders',
   imports: [],
   templateUrl: './allorders.component.html',
-  styleUrl: './allorders.component.scss'
+  styleUrl: './allorders.component.scss',
 })
 export class AllordersComponent {
+  constructor(private productsService: ProductsService) {}
+  moviesList: any[] = [];
 
+  @ViewChild('searchInput') searchInput!: ElementRef;
+
+  ngAfterViewInit(): void {
+    console.log(this.searchInput.nativeElement);
+
+    fromEvent(this.searchInput.nativeElement, 'keyup')
+      .pipe(
+        distinctUntilChanged(),
+        debounceTime(2000),
+        switchMap((res: any) => {
+          return this.productsService.getMoviesDatat(res.target.value);
+        })
+      )
+
+      .subscribe({
+        next: (res) => {
+          console.log(res.Search);
+          this.moviesList = res.Search;
+        },
+      });
+  }
 }
